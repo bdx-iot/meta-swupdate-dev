b5ac7cbd6c0486ef3d997d0d6a8991f109c9d9a0
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-05-01 13:29:50 +0200

    warp7/kernel: generate also fitImage
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>

--- recipes-bsp/u-boot-scr/files/imx7s-warp/boot.cmd
+++ recipes-bsp/u-boot-scr/files/imx7s-warp/boot.cmd
@@ -1,5 +1,4 @@
 setenv bootargs 'console=${console},${baudrate} root=/dev/mmcblk1p'${rootpart}' rootwait rw'
 run swu_setup
-load mmc 0:${rootpart} ${loadaddr} /boot/${image}
-load mmc 0:${rootpart} ${fdt_addr} /boot/${fdt_file}
-bootz ${loadaddr} - ${fdt_addr}
+load mmc 0:${rootpart} 0x86000000 /boot/fitImage
+bootm 0x86000000
